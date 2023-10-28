1a813357ec3ac61bf959c61626e85ec28abeaba3
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-10-28 19:46:57 +0530

    Init StreamLit

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import streamlit as st
 from app.youtube_data import YouTubeData
 from app.sentiment_analyzer import SentimentAnalyzer
 
-url = "https://www.youtube.com/watch?v=hXdLpnvbixs"
+st.title("YouTube Sentiment Analyzer")
 
-dataset = YouTubeData(url)
+yt_url = st.text_input("Enter YouTube URL")
 
-# Collect YouTube video details
-info = dataset.get_video_info()
-comments = dataset.get_comments()
 
-sentiment_analyzer = SentimentAnalyzer(comments)
+if yt_url:
+    # Collect YouTube video details
+    dataset = YouTubeData(yt_url)
+    info = dataset.get_video_info()
 
-# Collect Sentiment Analysis report
-sentiments = sentiment_analyzer.analyze_sentiment()
-
-print(sentiments)
+# comments = dataset.get_comments()
+#
+# sentiment_analyzer = SentimentAnalyzer(comments)
+#
+# # Collect Sentiment Analysis report
+# sentiments = sentiment_analyzer.analyze_sentiment()
+#
+# print(sentiments)
