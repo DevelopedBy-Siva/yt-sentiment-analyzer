af15f694641af69ebb8d642605e91b2a72eebfe1
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-10-28 13:11:44 +0530

    Use BERT model and analyze the result

--- app/sentiment_analyzer.py
+++ app/sentiment_analyzer.py
@@ -5,3 +5,18 @@ class SentimentAnalyzer:
 
     def __init__(self, data):
         self.data = data
+        self.sentiment_analysis = pipeline("sentiment-analysis")
+
+    def analyze_sentiment(self):
+        sentiments = []
+
+        for comment_data in self.data:
+            comment_text = comment_data['comment']['text']
+            comment_sentiment = self.sentiment_analysis(comment_text)
+            sentiments.append({
+                'text': comment_text,
+                'sentiment': comment_sentiment[0]['label'],
+                'confidence': comment_sentiment[0]['score']
+            })
+
+        return sentiments
--- main.py
+++ main.py
@@ -5,7 +5,13 @@ url = "https://www.youtube.com/watch?v=hXdLpnvbixs"
 
 dataset = YouTubeData(url)
 
+# Collect YouTube video details
 info = dataset.get_video_info()
 comments = dataset.get_comments()
 
-sentiment = SentimentAnalyzer(comments)
+sentiment_analyzer = SentimentAnalyzer(comments)
+
+# Collect Sentiment Analysis report
+sentiments = sentiment_analyzer.analyze_sentiment()
+
+print(sentiments)
