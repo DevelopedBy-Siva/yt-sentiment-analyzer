53c7eaa855fb6ded73e7ba7144aba952a623dead
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-10-28 13:36:45 +0530

    Use ROBERTA model and analyze

--- app/sentiment_analyzer.py
+++ app/sentiment_analyzer.py
@@ -1,22 +1,30 @@
-from transformers import pipeline
+from transformers import pipeline, RobertaForSequenceClassification, RobertaTokenizer
 
 
 class SentimentAnalyzer:
 
     def __init__(self, data):
         self.data = data
-        self.sentiment_analysis = pipeline("sentiment-analysis")
+        self.tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+        self.model = RobertaForSequenceClassification.from_pretrained("roberta-base")
+        self.sentiment_analysis = pipeline("sentiment-analysis", model=self.model, tokenizer=self.tokenizer)
 
     def analyze_sentiment(self):
         sentiments = []
 
         for comment_data in self.data:
             comment_text = comment_data['comment']['text']
-            comment_sentiment = self.sentiment_analysis(comment_text)
+            inputs = self.tokenizer(comment_text, return_tensors="pt")
+            outputs = self.model(**inputs)
+            logits = outputs.logits
+            predicted_class = logits.argmax().item()
+            sentiment_label = "POSITIVE" if predicted_class == 1 else "NEGATIVE"
+            confidence = logits.softmax(dim=1)[0][predicted_class].item()
+
             sentiments.append({
                 'text': comment_text,
-                'sentiment': comment_sentiment[0]['label'],
-                'confidence': comment_sentiment[0]['score']
+                'sentiment': sentiment_label,
+                'confidence': confidence
             })
 
         return sentiments
