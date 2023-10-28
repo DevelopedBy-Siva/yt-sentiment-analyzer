25a6a5cd02c53c1002e3d6109eb722182682cc6d
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-10-28 12:20:07 +0530

    Add doc-string

--- app/youtube_data.py
+++ app/youtube_data.py
@@ -27,10 +27,27 @@ def extract_video_id(url):
 class YouTubeData:
 
     def __init__(self, url):
+        """
+        :param url: YouTube Video URL
+        """
         self.youtube = build('youtube', 'v3', developerKey=API_KEY)
         self.video_id = extract_video_id(url)
 
     def get_video_info(self):
+        """
+        Get details for a YouTube video.
+
+        :return: A dictionary containing video information.
+            Keys:
+            - 'video_id': The ID of the video.
+            - 'video_name': The title of the video.
+            - 'channel_title': The title of the channel that uploaded the video.
+            - 'likes': The number of likes on the video.
+            - 'dislikes': The number of dislikes on the video.
+            - 'views': The number of views on the video.
+
+        :raises VideoNotFoundError: If the video does not exist.
+        """
         video_info = self.youtube.videos().list(
             part='snippet,statistics',
             id=self.video_id
@@ -49,11 +66,13 @@ class YouTubeData:
         channel_title = snippet.get('channelTitle', 'Unknown Channel')
         like_count = int(statistics.get('likeCount', 0))
         dislike_count = int(statistics.get('dislikeCount', 0))
+        view_count = int(statistics.get('viewCount', 0))
 
         return {
             'video_id': self.video_id,
             'video_name': video_name,
             'channel_title': channel_title,
             'likes': like_count,
-            'dislikes': dislike_count
+            'dislikes': dislike_count,
+            'views': view_count
         }
